7448c1a975feab27193bf395aaeb025dc8d48663
Aleksandra Batanova <abatanova@intermedia.com>
2025-10-16 16:02:21 +0200

    port changes

--- src/app.py
+++ src/app.py
@@ -12,5 +12,5 @@ def index():
     return render_template("index.html", cat_url=cat_url)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
 
